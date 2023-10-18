7bdc450117f875eae05fffd76d4a157354eecc95
Jay <jaybhesania@gmail.com>
2023-10-18 00:28:29 -0400

    Updated app.py file

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=['GET', 'POST'])
 def index():
-    return "Hey, this is my first ML Project"
+    return "CI CD pipeline has been established."
 
 
 if __name__=="__main__":
