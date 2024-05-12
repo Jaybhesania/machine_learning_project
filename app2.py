8e299a440e041dfbfa1cb0592bdd49c24f871764
Jay <jaybhesania@gmail.com>
2024-05-11 23:58:43 -0400

    new_secrets

--- app2.py
+++ app2.py
@@ -4,9 +4,7 @@ app=Flask(__name__)
 
 @app. route("/", methods=[ 'GET', 'POST'])
 def index():
-    return "Starting Machine Learning Project"
+    return "Starting Machine Learning Projects"
 
 if __name__=="__main__":
     app.run(debug=True)
-
-    
